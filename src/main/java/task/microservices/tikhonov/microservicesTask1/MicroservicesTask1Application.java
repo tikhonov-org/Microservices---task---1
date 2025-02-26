a84039a2e45670dc6161437ed4e9b4f3a9e5832d
george <e.tihonov@websoft.ru>
2025-02-26 22:32:55 +0300

    Initial commit

---
+++ src/main/java/task/microservices/tikhonov/microservicesTask1/MicroservicesTask1Application.java
@@ -0,0 +1,13 @@
+package task.microservices.tikhonov.microservicesTask1;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MicroservicesTask1Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MicroservicesTask1Application.class, args);
+	}
+
+}
---
+++ src/test/java/task/microservices/tikhonov/microservicesTask1/MicroservicesTask1ApplicationTests.java
@@ -0,0 +1,13 @@
+package task.microservices.tikhonov.microservicesTask1;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MicroservicesTask1ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
